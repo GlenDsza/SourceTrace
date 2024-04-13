7f675c0700037e1f85724c0e42958c3fe18137d2
Glen <dsouzaglen30@gmail.com>
2024-04-13 17:41:11 +0530

    Migrate to Amoy testnet

--- driver/accounts.js
+++ driver/accounts.js
@@ -1,8 +1,8 @@
-const Web3 = require('web3');
-const { producerAccounts, warehouseAccounts } = require('./details');
-const { extraTrucksPk } = require('./extraTrucks');
+const Web3 = require("web3");
+const { producerAccounts, warehouseAccounts } = require("./details");
+const { extraTrucksPk } = require("./extraTrucks");
 
-const providerURL = `https://rpc-mumbai.maticvigil.com/`;
+const providerURL = `https://rpc-amoy.polygon.technology`;
 
 const customWeb3 = new Web3(providerURL);
 
@@ -13,23 +13,23 @@ Object.entries(producerAccounts).forEach(([_, producer]) => {
   customWeb3.eth.accounts.wallet.add(
     customWeb3.eth.accounts.privateKeyToAccount(producer.pk)
   );
-  producer.trucks?.forEach(truck => {
+  producer.trucks?.forEach((truck) => {
     const account = customWeb3.eth.accounts.privateKeyToAccount(truck.pk);
     customWeb3.eth.accounts.wallet.add(account);
     truckAccounts.push(account.address);
-  })
+  });
 });
 Object.entries(warehouseAccounts).forEach(([_, warehouse]) => {
   customWeb3.eth.accounts.wallet.add(
     customWeb3.eth.accounts.privateKeyToAccount(warehouse.pk)
   );
-  warehouse.trucks?.forEach(truck => {
+  warehouse.trucks?.forEach((truck) => {
     const account = customWeb3.eth.accounts.privateKeyToAccount(truck.pk);
     customWeb3.eth.accounts.wallet.add(account);
     truckAccounts.push(account.address);
-  })
+  });
 });
-extraTrucksPk.forEach(pk => {
+extraTrucksPk.forEach((pk) => {
   const account = customWeb3.eth.accounts.privateKeyToAccount(pk);
   customWeb3.eth.accounts.wallet.add(account);
   truckAccounts.push(account.address);
--- src/contracts/deploymentDetails.ts
+++ src/contracts/deploymentDetails.ts
@@ -1,6 +1,6 @@
 // export const contractDeploymentTx = "0x383e2c398f766d461c077125d52ac60121a35a3ef23c5a6853e6a1f1a04685e5";
-// export const contractDeploymentTxLink = `${chainExplorerBaseAddress}/tx/${contractDeploymentTx}`; 
+// export const contractDeploymentTxLink = `${chainExplorerBaseAddress}/tx/${contractDeploymentTx}`;
 
-export const contractAddress = "0xf62Bb02bAf1E461C9c9ff7071df64e10a0846cA5";
+export const contractAddress = "0x2b91100c50c04126dbdbd71e873539bda4331b11";
 
-export const chainExplorerBaseAddress = "https://mumbai.polygonscan.com";
+export const chainExplorerBaseAddress = "https://www.oklink.com/amoy";
